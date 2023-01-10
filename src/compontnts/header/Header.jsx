87132b0603d969e874bf74867734a6e8f4eb79f6
liza <liza@lizas-MacBook-Air.local>
2023-01-10 23:42:40 +0300

    setup gh-pages

---
+++ src/compontnts/header/Header.jsx
@@ -0,0 +1,15 @@
+import Button from './ux/Button'
+
+function Header() {
+  return (
+    <header className="Header flex container">
+     <img src='./logo.svg' alt="" />
+        <div className="header__wrapper flex">
+            <p className="header-wrapper__phone">+7 (456) 567-67-67</p>
+            <Button class={'header-wrapper__button'} title={'Записаться на урок'}></Button>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/compontnts/header/ux/Button.jsx
@@ -0,0 +1,9 @@
+
+function Button(props) {
+    return (
+        <button className={`pruple-button ${props.class}`}>{props.title}</button>
+    );
+  }
+  
+  export default Button;
+  
